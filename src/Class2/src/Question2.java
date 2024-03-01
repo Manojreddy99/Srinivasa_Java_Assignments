23a5b4ff97913e3e069f36b46ad4dab5154e693c
Manojreddy99 <tsmanojreddy99@gmail.com>
2024-03-01 10:26:04 -0600

    class2 programs

---
+++ src/Class2/src/Question2.java
@@ -0,0 +1,12 @@
+public class Question2{
+    public static void main(String args[])
+    {
+        int x = 3;
+        int y;
+        y = x++;
+        if(x!=y){
+            System.out.println(x);
+            System.out.println(y);
+        }
+    }
+}
