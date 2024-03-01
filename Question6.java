4d86efc4864ddd1b8f62df568004db66b43e59f8
Manojreddy99 <58143429+Manojreddy99@users.noreply.github.com>
2024-03-01 09:20:06 -0600

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,13 @@
+public class Question6{
+    public static void main(String args[])
+    {
+        boolean b = false;
+
+        if(b==true){
+            System.out.println("b is True");
+        }
+        else{
+            System.out.println("b is False");
+        }
+    }
+}
